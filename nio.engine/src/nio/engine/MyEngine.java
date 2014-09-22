7fc95866012edd8565b5b06af880c864f79c1fea
Paul <paul.mariage@live.fr>
2014-09-22 12:20:56 +0200

    Ajout du selector et des handles

--- nio.engine/src/nio/engine/MyEngine.java
+++ nio.engine/src/nio/engine/MyEngine.java
@@ -3,26 +3,29 @@ package nio.engine;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Set;
-
 import javax.security.auth.callback.CallbackHandler;
 
 public class MyEngine extends NioEngine {
 
 	protected CallbackHandler myCallbackHander;
 	Selector selector;
-
+	// buffers for writing data
+	Hashtable<SocketChannel,ByteBuffer> outBuffers;
+		
 	public MyEngine(int port) throws Exception {
 		super();
-		this.selector = Selector.open();
+		this.selector = SelectorProvider.provider().openSelector();
 	}
 
 	/**
@@ -33,40 +36,36 @@ public class MyEngine extends NioEngine {
 	@Override
 	public void mainloop() {
 		while (true) {
-
-			int readyChannels = 0;
 			try {
-				readyChannels = selector.select();
-			} catch (IOException e) {
-				System.out.println("Probleme select du selector : "
-						+ e.getMessage());
-			}
 
-			if (readyChannels == 0)
-				continue;
+				selector.select();
 
-			Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<?> selectedKeys = this.selector.selectedKeys().iterator();
 
-			Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+				while (selectedKeys.hasNext()) {
 
-			while (keyIterator.hasNext()) {
+					SelectionKey key = (SelectionKey) selectedKeys.next();
+					selectedKeys.remove();
 
-				SelectionKey key = keyIterator.next();
+					if (!key.isValid()) {
+						continue;
 
-				if (key.isAcceptable()) {
-					// a connection was accepted by a ServerSocketChannel.
+					} else if (key.isAcceptable()) {
+						handleAccept(key);
 
-				} else if (key.isConnectable()) {
-					// a connection was established with a remote server.
+					} else if (key.isReadable()) {
+						handleRead(key);
 
-				} else if (key.isReadable()) {
-					// a channel is ready for reading
+					} else if (key.isWritable()) {
+						handleWrite(key);
 
-				} else if (key.isWritable()) {
-					// a channel is ready for writing
+					} else if (key.isConnectable()) {
+						handleConnection(key);
+					} else 
+						System.out.println("  ---> unknow key=");
 				}
-
-				keyIterator.remove();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
@@ -93,7 +92,11 @@ public class MyEngine extends NioEngine {
 		SocketChannel sc = channel.getChannel();
 		sc = serverChannel.accept();
 		if (sc != null)
+		{
+			sc.register(selector, SelectionKey.OP_ACCEPT);
 			callback.accepted(server, channel);
+		}
+			
 
 		return server;
 	}
@@ -121,10 +124,145 @@ public class MyEngine extends NioEngine {
 		InetSocketAddress isa = new InetSocketAddress(hostAddress, port);
 		mySocketChannel.bind(isa);
 		mySocketChannel.connect(isa);
+		
 		if (mySocketChannel.finishConnect())
 		{
+			//Si la connexion a reussi
 			System.out.println("Connecté à la socket");
+			mySocketChannel.register(selector, SelectionKey.OP_CONNECT);
 			callback.connected(channel);
 		}
+	}
+	
+	/**
+	 * Accept a connection and make it non-blocking
+	 * @param the key of the channel on which a connection is requested
+	 */
+	private void handleAccept(SelectionKey key) {
+		System.out.println("Server[HandleAccept]");
+		SocketChannel socketChannel = null;
+		ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+		try {
+			socketChannel = serverSocketChannel.accept();
+			socketChannel.configureBlocking(false);
+		} catch (IOException e) {
+			// as if there was no accept done
+			return;
+		}
+
+		// be notified when there is incoming data 
+		try {
+			socketChannel.register(this.selector, SelectionKey.OP_READ);
+		} catch (ClosedChannelException e) {
+			handleClose(socketChannel);
+		}
+		outBuffers.put(socketChannel, ByteBuffer.allocate(128));
+	}
+
+
+	/**
+	 * Finish to establish a connection
+	 * @param the key of the channel on which a connection is requested
+	 */
+	private void handleConnection(SelectionKey key) {
+		System.out.println("Server[HandleConnection]");
+		SocketChannel socketChannel = (SocketChannel) key.channel();
+
+		try {
+			socketChannel.finishConnect();
+		} catch (IOException e) {
+			// cancel the channel's registration with our selector
+			System.out.println(e);
+			key.cancel();
+			return;
+		}
+		key.interestOps(SelectionKey.OP_READ);	
+	}
+
+
+	/**
+	 * Close a channel 
+	 * @param the key of the channel to close
+	 */
+	private void handleClose(SocketChannel socketChannel) {
+		System.out.println("Server[HandleClose]");
+		try{
+			socketChannel.close();
+		} catch (IOException e) {
+			// nothing to do, the channel is already closed
+		}
+		socketChannel.keyFor(selector).cancel();
+	}
+
+	/**
+	 * Handle incoming data event
+	 * @param the key of the channel on which the incoming data waits to be received 
+	 */
+	private void handleRead(SelectionKey key) throws IOException{
+		
+		SocketChannel sChannel = (SocketChannel) key.channel();
+		ByteBuffer inBuffer = ByteBuffer.allocate(512);
+		
+		int nbRead = 0;
+		
+		try {
+			nbRead = sChannel.read(inBuffer);
+		} catch (IOException e) {
+			key.cancel();
+			sChannel.close();
+			System.out.println(e);
+			return;
+		}
+		
+		if(nbRead == -1) {
+			key.channel().close();
+			key.cancel();
+			return;
+		}
+		
+		System.out.println("Server[HandleRead]--> "+inBuffer.toString());
+		send(key, inBuffer.array());
+		
+	}
+
+
+	/**
+	 * Handle outgoing data event
+	 * @param the key of the channel on which data can be sent 
+	 * @throws IOException 
+	 */
+	private void handleWrite(SelectionKey key) throws IOException {
+		
+		SocketChannel sChannel = (SocketChannel) key.channel();
+
+		ByteBuffer outBuffer = outBuffers.get(sChannel);
+		
+		if(outBuffer.remaining() > 0) {
+
+			int nbWrite = 0;
+			try {
+				nbWrite = sChannel.write(outBuffer);
+			} catch (IOException e) {
+				key.cancel();
+				sChannel.close();
+				e.printStackTrace();
+				return;
+			}
+
+		}
+		
+		
+		System.out.println("Server[HandleWrite]--> "+outBuffer.toString());
+		outBuffer.clear();
+		key.interestOps(SelectionKey.OP_READ);
+		
+	}
+	
+public void send(SelectionKey key, byte[] data) {
+		
+		ByteBuffer outBuffer = outBuffers.get(key.channel());
+		outBuffer = ByteBuffer.wrap(data);
+		key.interestOps(SelectionKey.OP_WRITE);
+		
 	}
 }
